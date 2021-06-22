e84dada97c3e6549d007ab6e4d30ae5e0dffdf96
shubham-1611 <70129271+shubham-1611@users.noreply.github.com>
2021-06-22 12:48:06 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,6 +3,14 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
+            try{
+                if(n<0)
+                  throw new ArithmeticException("NUmber invalid");
+              }
+              catch(Exception e)
+              {
+                System.out.println("Number not valid");
+              }
     String s="";
             while(n!=0)
             {
